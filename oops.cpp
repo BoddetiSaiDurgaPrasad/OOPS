faa998b29bb3393584e94fef347ff74609317e61
BoddetiSaiDurgaPrasad <94205751+BoddetiSaiDurgaPrasad@users.noreply.github.com>
2022-11-12 14:16:52 +0530

    oops upto inheritance
    oops upto inheritance

--- oops.cpp
+++ oops.cpp
@@ -21,7 +21,18 @@ public:
 		3.copy constructor // if we want to create the a new object
 		with older object then the copy constructor will be used 
 	*/
-
+	/*
+	Method Over riding parent class lo and child class lo same methods vunnapudu
+	the if we create a object with child class then it will go to the child class
+	but if we want to go to the parent class with child object then 
+	childobject.parent class name::methodname;
+	*/
+	/*
+	Method Overloading 
+	if we want a differnt data type to be printed every time
+	we have to pass diffent arguments but here we will write diff methods 
+	with different datatypes and all these will be only in one class 
+	*/
 	int AniCount;  // these three are attributes
 	int TreeCount; //attribute2
 	string Name;  // attribute 3
@@ -56,6 +67,16 @@ class cuboid {
 // child class or derived class
 // base class data ni access cheyyochhu
 }
+/*
+if chlid has single class
+class childclass : public parent class name{
+	
+} 
+if child has multiple class
+class childclass : public parent1classname : public parent2classname{
+	
+} 
+*/
 int main(){
 Adivi h1(20,30,"amazon");
 
